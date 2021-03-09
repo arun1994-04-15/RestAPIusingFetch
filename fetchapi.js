a3043a38aabc7ec3c667c05ba1b22eab80cf26e3
arun1994-04-15 <69531101+arun1994-04-15@users.noreply.github.com>
2021-03-09 21:41:13 +0530

    Update fetchapi.js

--- fetchapi.js
+++ fetchapi.js
@@ -1,21 +1,21 @@
-var data = fetch("https://restcountries.eu/rest/v2/all")
+var data = fetch("https://restcountries.eu/rest/v2/all");
 
-data.then(function(response){
-    return response.json()
-})
-.then(function(obj){
-    dispalycountries(obj)
-})
-.catch (function(err){
-    console.log(err)
-})
+data
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (obj) {
+    dispalycountries(obj);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
+function dispalycountries(obj) {
+  var countries = document.getElementById("country");
 
-function dispalycountries(obj){
-    var countries = document.getElementById('country');
-
-    for (var i=0; i<obj.length ;i++){
-        var card =`<div class="col-md-4 col-sm-6">
+  for (var i = 0; i < obj.length; i++) {
+    var card = `<div class="col-md-4 col-sm-6">
         <div class="card-group" style="width:15rem" >
           <img src="${obj[i].flag}" class="card-img-top" alt="...">
           <div class="card-body">
@@ -23,13 +23,23 @@ function dispalycountries(obj){
             <p class="card-capital">Population: ${obj[i].population}</p>
             <p class="card-capital">Capital: ${obj[i].capital}</p>
             <p class="card-capital">Region: ${obj[i].region}</p>
+            <button onclick="weather(${obj[i].name})" class="btn btn-outline-danger"> Weather </button>
           </div>
         </div>
-      </div>`
-      countries.innerHTML += card;
-    }
-
+      </div>`;
+    countries.innerHTML += card;
     
+  }
 }
 
+function weather(countryname){
+  var data1 = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${countryname}&appid=0d33e53012116e5f0c14e4f77f0ccd0f`);
+  data1
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (obj1){
+    alert(obj1.weather[0].main)
+  })
+  }
 
